4435e8308db3642b083f716d079e40523ad61540
crism.dev <crism.dev@gmail.com>
2023-04-10 10:52:14 +0800

    usage
    test proc usage

---
+++ multi_threaded/test.py
@@ -0,0 +1,13 @@
+# SAMPLE SCRIPT CALL:
+
+commands = [
+    '/user/magalang/bin/run_mk_2011',
+    '/user/magalang/bin/run_mk_2012',
+    '/user/magalang/bin/run_mk_2013'
+]
+
+def post_processing():
+    # Perform post processing operation
+    pass
+
+run_commands(commands, post_processing)
